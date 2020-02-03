c7cd5ee9dbe5e0c0718ca463c5cc345c75b08d0c
Shmily-2375 <1195852440@qq.com>
2020-02-03 15:24:32 +0800

    'final'

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store";
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 Vue.config.productionTip = false
 
 //添加事件总线对象
 Vue.prototype.$bus = new Vue()
+
+//解决移动端300ms的延迟
+FastClick.attach(document.body)
+
+//使用懒加载插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
